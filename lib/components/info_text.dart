9613ef6a5277112cd5f687bff040b40e883d61f3
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-12-23 19:40:37 +0900

    feat(info_text.dart): remove Navigator.push -> MapPage.dart
    because AlbumArt does have it

--- lib/components/info_text.dart
+++ lib/components/info_text.dart
@@ -1,5 +1,4 @@
 import 'package:aoku/models/audio_state.dart';
-import 'package:aoku/pages/map_page.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
@@ -13,61 +12,50 @@ class InfoText extends HookConsumerWidget {
   Widget build(BuildContext context, WidgetRef ref) {
     AudioState audioState = ref.watch(audioProvider);
 
-    return GestureDetector(
-      onTap: () => Navigator.push(
-        context,
-        MaterialPageRoute(
-          builder: (context) => MapPage(
-            initialLocation:
-                audioState.sounds[audioState.currentIndex].location,
-          ),
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              CupertinoIcons.map_pin_ellipse,
+              size: 12.0,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            const SizedBox(width: 4.0),
+            Text(
+              audioState.initStatus == AudioStateInitStatus.done
+                  ? '${audioState.sounds[audioState.currentIndex].city}, ${audioState.sounds[audioState.currentIndex].province}'
+                  : 'Loading...',
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onBackground,
+                fontSize: 12.0,
+              ),
+            ),
+          ],
         ),
-      ),
-      child: Column(
-        children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(
-                CupertinoIcons.map_pin_ellipse,
-                size: 12.0,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Icon(
+              CupertinoIcons.calendar_today,
+              size: 12.0,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
+            const SizedBox(width: 4.0),
+            Text(
+              audioState.initStatus == AudioStateInitStatus.done
+                  ? audioState.sounds[audioState.currentIndex].time
+                      .format(context)
+                  : 'Loading...',
+              style: TextStyle(
                 color: Theme.of(context).colorScheme.onBackground,
+                fontSize: 12.0,
               ),
-              const SizedBox(width: 4.0),
-              Text(
-                audioState.initStatus == AudioStateInitStatus.done
-                    ? '${audioState.sounds[audioState.currentIndex].city}, ${audioState.sounds[audioState.currentIndex].province}'
-                    : 'Loading...',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onBackground,
-                  fontSize: 12.0,
-                ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Icon(
-                CupertinoIcons.calendar_today,
-                size: 12.0,
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
-              const SizedBox(width: 4.0),
-              Text(
-                audioState.initStatus == AudioStateInitStatus.done
-                    ? audioState.sounds[audioState.currentIndex].time
-                        .format(context)
-                    : 'Loading...',
-                style: TextStyle(
-                  color: Theme.of(context).colorScheme.onBackground,
-                  fontSize: 12.0,
-                ),
-              ),
-            ],
-          ),
-        ],
-      ),
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
