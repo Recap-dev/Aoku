e89f9e9282d0b9bd6c0fe2b89ffc6d16af183f37
Yusuke Hakamaya <yhakamay@student.42tokyo.jp>
2021-12-26 15:44:42 +0900

    feat(shuffle_to_play_button.dart): looks great!

--- lib/components/shuffle_to_play_button.dart
+++ lib/components/shuffle_to_play_button.dart
@@ -17,28 +17,58 @@ class ShuffleToPlayButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.end,
-      children: [
-        IconButton(
-          onPressed: () async {
-            HapticFeedback.lightImpact();
-            await audioState.toggleShuffleMode(forceEnable: true);
-            await audioState.toggleLoopMode(forceEnable: true);
-            showCupertinoModalBottomSheet(
-              context: context,
-              builder: (context) => const PlayPage(),
-            );
-            audioState.play(
-              Random().nextInt(audioState.sounds.length),
-            );
-          },
-          icon: Icon(
-            CupertinoIcons.shuffle,
-            color: Theme.of(context).colorScheme.onBackground,
+    return IconButton(
+      onPressed: () async {
+        HapticFeedback.lightImpact();
+        await audioState.toggleShuffleMode(forceEnable: true);
+        await audioState.toggleLoopMode(forceEnable: true);
+        showCupertinoModalBottomSheet(
+          context: context,
+          builder: (context) => const PlayPage(),
+        );
+        audioState.play(
+          Random().nextInt(audioState.sounds.length),
+        );
+      },
+      iconSize: 48.0,
+      icon: Stack(
+        children: [
+          Container(
+            width: 48.0,
+            height: 48.0,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Theme.of(context).colorScheme.onBackground,
+            ),
           ),
-        ),
-      ],
+          Align(
+            alignment: const Alignment(0.3, 0.1),
+            child: Icon(
+              CupertinoIcons.play_fill,
+              color: Theme.of(context).colorScheme.surface,
+              size: 30.0,
+            ),
+          ),
+          Align(
+            alignment: const Alignment(1.2, 1.2),
+            child: Container(
+              width: 16.0,
+              height: 16.0,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Theme.of(context).colorScheme.surface,
+              ),
+              child: Center(
+                child: Icon(
+                  CupertinoIcons.shuffle,
+                  color: Theme.of(context).colorScheme.onBackground,
+                  size: 8.0,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
